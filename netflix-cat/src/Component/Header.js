744f61daee036b573c8629cc4b644496fedfe7f4
Harsh Kumar <9u.harsh@gmail.com>
2022-09-22 00:08:00 +0530

    netflix clone on go

--- netflix-cat/src/Component/Header.js
+++ netflix-cat/src/Component/Header.js
@@ -11,24 +11,31 @@ export default function Header(){
       const req = await axios.get(fetchHead);
       const results = req.data.results;
       const len = req.data.results.length;
-      getHeadMovie(results[Math.floor(Math.random()*(len-1))]);
+      getHeadMovie(results[Math.floor(Math.random()*(len+1))]);
     }
     getHeadMovies();
   }, [fetchHead]);
   console.table(headMovie);
     return(
         <>
-            <header style={{backgroundImage:`url("https://image.tmdb.org/t/p/original/${headMovie.poster_path}")`}}>
-            <div>
-                <p>Netflix</p>
+            <header style={{backgroundImage:`url("https://image.tmdb.org/t/p/original/${headMovie?.poster_path}")`}}>
+            <nav>
+                <h1>Netflix</h1>
                 <ul>
                     <li><button>Sign In</button></li>
                     <li><button>Sign Up</button></li>
                 </ul>
-            </div>
+            </nav>
             <div className='detail'>
-                <p>{headMovie.name}</p>
-                <p>{headMovie.overview}</p>
+                <h1>{headMovie?.name}</h1>
+                <ul>
+                  <li><button>Play</button></li>
+                  <li><button>Watch Later</button></li>
+                </ul>
+                <div>
+                <p>{headMovie?.first_air_date}</p>
+                <p>{(headMovie?.overview?.length > 100) ? (`${headMovie.overview.slice(0,100)} . . . .`) : (headMovie.overview)}</p>
+                </div>
             </div>
             </header>
         </>
--- netflix-cat/src/Component/Reel.js
+++ netflix-cat/src/Component/Reel.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Youtube from "react-youtube";
+import movieTrailer from 'movie-trailer'; 
 
 export default function Reel({ head, lnk }) {
   const [movieList, getList] = useState([]);
+  const [showTrailer , getTrailer] = useState("");
   const fetchLink = `https://api.themoviedb.org/3/${lnk}`;
 
   useEffect(() => {
@@ -12,7 +15,26 @@ export default function Reel({ head, lnk }) {
     }
     getMovies();
   }, [fetchLink]);
-  console.table(movieList);
+  function callMovie(smovie){
+    if(showTrailer){
+      getTrailer("");
+    }else{
+      
+      movieTrailer(smovie?.name || smovie?.title || "")
+      .then((url)=>{
+        const urlParameters = new URLSearchParams(new URL(url).search);
+        getTrailer(urlParameters.get('v'));
+      })
+      .catch();
+    }
+  }
+  const opts = {
+    height: "350",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <>
       <section>
@@ -20,14 +42,23 @@ export default function Reel({ head, lnk }) {
         <div className="movie-list">
           {movieList.map((smovie, idx) => {
             return (
+              //onclick callback function 
+              <div className="movie-only" key={idx} onClick={()=>callMovie(smovie)}>
               <img
                 src={`https://image.tmdb.org/t/p/original/${smovie.poster_path}`}
-                alt={smovie.name || smovie.title}
-                key={idx}
+                alt={smovie?.name || smovie?.title}
+                
               />
+              <p>{smovie?.name || smovie?.title}</p>
+              </div>
             );
           })}
         </div>
+
+        {/* know more about the youtube npm package */}
+        <div>
+       {showTrailer && <Youtube  videoId={showTrailer} opts={opts} />}
+       </div>
       </section>
     </>
   );
