5e194b7354d58c0441da33960fde3a7260e20fcc
Harsh Kumar <9u.harsh@gmail.com>
2022-09-23 20:33:03 +0530

    amazon header design

---
+++ amazon-cat/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      bubblegum: "#ff77e9",
+      denim: "#0F1111",
+      srchC: "#f59e0b",
+    },
+  },
+  plugins: [],
+};
