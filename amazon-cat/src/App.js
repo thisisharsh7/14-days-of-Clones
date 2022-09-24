16381c836be87e4f5e5e46ccc456434696f1e3af
Harsh Kumar <9u.harsh@gmail.com>
2022-09-24 08:52:11 +0530

    amazon clone on go

--- amazon-cat/src/App.js
+++ amazon-cat/src/App.js
@@ -5,13 +5,29 @@ import SignIn from "./Components/SignIn";
 import { Routes, Route } from "react-router-dom";
 import Cart from "./Components/Cart";
 import { useState } from "react";
+import { useEffect } from "react";
+
+const showSavedCnt = () =>{
+  let savedCnt = localStorage.getItem("cart-cnt");
+  if(savedCnt !== null){
+    return JSON.parse(savedCnt);
+  }else{
+    return 0;
+  }
+}
 
 export default function App() {
-  const [setItemCnt , getItemCnt] = useState(0);
+  const [setItemCnt , getItemCnt] = useState(showSavedCnt);
+  const [setCart , getCart] = useState([]);
+  
   function AddToCart(e){
     getItemCnt(setItemCnt + 1);
-    console.log(e.target.parentElement.children[0])
+    getCart(e.target.parentElement.children[0]);
   }
+  useEffect(()=>{
+    localStorage.setItem("cart-cnt",JSON.stringify(setItemCnt));
+  },[setItemCnt])
+  console.log(setCart);
   return (
     <>
       <Routes>
