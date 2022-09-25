edf1bd0c17924f5f0b252a66f79fa13a8f3528bd
Harsh Kumar <9u.harsh@gmail.com>
2022-09-25 22:45:21 +0530

    spotify clone on go

---
+++ spotify-cat/pages/index.js
@@ -0,0 +1,5 @@
+export default function index() {
+  return (
+    <div><h1 className="bg-red-800">This is a start of spotify clone</h1></div>
+  )
+}
---
+++ spotify-cat/pages/login.js
@@ -0,0 +1,8 @@
+export default function login() {
+  return (
+    <div className="flex flex-col gap-12 items-center min-h-screen sm:justify-center justify-start p-4">
+      <img src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg" alt="spotify-logo" className="md:max-w-3xl sm:max-w-md " />
+      <button type="button" className="uppercase text-white bg-green-600 px-6 py-3 rounded-full font-bold sm:text-sm text-xs w-48">Login with spotify</button>
+    </div>
+  )
+}
