9297d94c24526c0aa2c4d94b05de12d1b3d2fbaa
Harsh Kumar <9u.harsh@gmail.com>
2022-10-04 20:46:18 +0530

    project on go

--- digital-ocean-cat/src/Components/Footer.js
+++ digital-ocean-cat/src/Components/Footer.js
@@ -3,11 +3,11 @@ import footerLogo from "./images/footerlogo.svg";
 
 export default function Footer() {
   return (
-    <footer>
-      <ul>
-        <li>
+    <footer className="bg-sky-600 text-white pt-8 pb-8">
+      <ul className="flex mx-auto max-w-screen-xl justify-between px-3 pb-8">
+        <li className="flex flex-col  font-bold cursor-auto gap-5 text-2xl ">
           Company
-          <ul>
+          <ul className="flex flex-col font-normal gap-3 items-start text-base">
             <li>About</li>
             <li>Leadership</li>
             <li>Blog</li>
@@ -22,9 +22,9 @@ export default function Footer() {
             <li>DO Impact</li>
           </ul>
         </li>
-        <li>
+        <li className="flex flex-col  font-bold cursor-text gap-5 text-2xl">
           Products
-          <ul>
+          <ul className="flex flex-col font-normal gap-3 items-start text-base">
             <li>Products Overview</li>
             <li>Droplets</li>
             <li>Kubernetes</li>
@@ -43,9 +43,9 @@ export default function Footer() {
             <li>Uptime</li>
           </ul>
         </li>
-        <li>
+        <li className="flex flex-col  font-bold cursor-text gap-5 text-2xl">
           Community
-          <ul>
+          <ul className="flex flex-col font-normal gap-3 items-start text-base">
             <li>Tutorials</li>
             <li>Q&A</li>
             <li>CSS-Tricks</li>
@@ -61,9 +61,9 @@ export default function Footer() {
             <li>Meetups</li>
           </ul>
         </li>
-        <li>
+        <li className="flex flex-col  font-bold cursor-text gap-5 text-2xl">
           Solutions
-          <ul>
+          <ul className="flex flex-col font-normal gap-3 items-start text-base">
             <li>Website Hosting</li>
             <li>VPS Hosting</li>
             <li>Web & Mobile Apps</li>
@@ -74,9 +74,9 @@ export default function Footer() {
             <li>Cloud Hosting for Blockchain</li>
           </ul>
         </li>
-        <li>
+        <li className="flex flex-col  font-bold cursor-text gap-5 text-2xl">
           Contanct
-          <ul>
+          <ul className="flex flex-col font-normal gap-3 items-start text-base">
             <li>Support</li>
             <li>Sales</li>
             <li>Report Abuse</li>
@@ -85,21 +85,66 @@ export default function Footer() {
           </ul>
         </li>
       </ul>
-      <div>
-        <div>
-            <img src={footerLogo} alt="footerLogo"/>
-            <p>© 2022 DigitalOcean, LLC. All rights reserved.</p>
+      <div className="flex mx-auto max-w-screen-xl justify-between px-3 items-center pt-8 border-t-2 border-dotted">
+        <div className="flex gap-2 items-center ">
+          <img src={footerLogo} alt="footerLogo " />
+          <p>© 2022 DigitalOcean, LLC. All rights reserved.</p>
         </div>
-        <ul>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialTwitch.a40b5940.svg" alt="twitch" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialTwitter.26ed7e61.svg" alt="twitter" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialFb.c716d8b2.svg" alt="twitter" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialInstagram.5fb5ecbd.svg" alt="twitter" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialYoutube.3ea36203.svg" alt="twitter" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialLinkedin.7662d59b.svg" alt="twitter" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialDev.c2375b02.svg" alt="twitter" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialGlassdoor.b7323088.svg" alt="twitter" /></li>
-            <li><img src="https://www.digitalocean.com/_next/static/media/socialBuiltinnyc.39adf06d.svg" alt="twitter" /></li>
+        <ul className="flex footer-icon gap-4 ">
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialTwitch.a40b5940.svg"
+              alt="twitch"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialTwitter.26ed7e61.svg"
+              alt="twitter"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialFb.c716d8b2.svg"
+              alt="fb"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialInstagram.5fb5ecbd.svg"
+              alt="instagram"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialYoutube.3ea36203.svg"
+              alt="youtube"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialLinkedin.7662d59b.svg"
+              alt="linkedin"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialDev.c2375b02.svg"
+              alt="dev"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialGlassdoor.b7323088.svg"
+              alt="glassdoor"
+            />
+          </li>
+          <li>
+            <img
+              src="https://www.digitalocean.com/_next/static/media/socialBuiltinnyc.39adf06d.svg"
+              alt="builtinny"
+            />
+          </li>
         </ul>
       </div>
     </footer>
